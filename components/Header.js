1b74caa9e3f4561e95041ff9ec30767a3f96b59a
amills25 <awmills25@gmail.com>
2022-10-25 14:46:24 -0400

    first commit

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
+      {/* left */}
+      <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+        <Image
+          src='https://links.papareact.com/qd3'
+          layout='fill'
+          objectFit='contain'
+          objectPosition='left'
+        />
+      </div>
+
+      {/* middle -- search */}
+      <div>
+        <input type="text" />
+      </div>
+
+      {/* right */}
+      <div></div>
+    </header>
+  )
+}
+
+export default Header
