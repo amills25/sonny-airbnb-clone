545ae8c1739ce475609a0fcfd7c3deb0f0204d60
amills25 <awmills25@gmail.com>
2022-10-25 17:19:40 -0400

    Through 1:36:00

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import { MagnifyingGlassIcon, GlobeAltIcon, Bars3Icon, UserCircleIcon, UserIcon  } from '@heroicons/react/24/outline'
 
 function Header() {
   return (
@@ -8,19 +9,29 @@ function Header() {
       <div className='relative flex items-center h-10 cursor-pointer my-auto'>
         <Image
           src='https://links.papareact.com/qd3'
-          layout='fill'
           objectFit='contain'
           objectPosition='left'
+          width={100}
+          height={40}
         />
       </div>
 
       {/* middle -- search */}
-      <div>
-        <input type="text" />
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input type="text" placeholder="Start your search" className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400' />
+        <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
 
       {/* right */}
-      <div></div>
+      <div className='flex space-x-4 items-center justify-end text-gray-500'>
+        <p className='hidden md:inline cursor-pointer'>Become a host</p>
+        <GlobeAltIcon className='h-6 cursor-pointer' />
+
+        <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+          <Bars3Icon className='h-6' />
+          <UserCircleIcon className='h-6' />
+        </div>
+      </div>
     </header>
   )
 }
